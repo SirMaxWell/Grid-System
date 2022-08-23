3bdeb0c95f024c81d1afc8043724a2bad5a9a330
SirMaxWell <50089794+SirMaxWell@users.noreply.github.com>
2022-08-23 12:16:24 +1000

    Basic Node Setup
    Spawns Nodes on top of the grid
    Going to use them for pathfinding

--- Assets/Scripts/GridGenerator.cs
+++ Assets/Scripts/GridGenerator.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class GridGenerator : MonoBehaviour
 {
     public GameObject gridPrefab; // the object that will be used to create the grid // ex cube 
+    public GameObject nodePrefab;
     public int gridZ; 
     public int gridX;
+
+    public float nodeRadius;
+    public float nodeDiameter;
     public float gridSpacingOffset = 1f; // how separated the user wants the grid 
     public Vector3 gridOrgin = Vector3.zero; // uses a set orgin instead of the position of prefab // more flexible 
 
     // Start is called before the first frame update
     void Start()
     {
+        
+        
         CreateGrid();
     }
 
@@ -25,8 +31,10 @@ public class GridGenerator : MonoBehaviour
             for (int z = 0; z < gridZ; z++)
             {
                 // creates the position for each grid which is passed to spawnGrid function 
-                Vector3 spawnPosition = new Vector3(x * gridSpacingOffset,0, z * gridSpacingOffset) + gridOrgin;
-                spawnGrid(spawnPosition, Quaternion.identity);
+                Vector3 spawnGridPosition = new Vector3(x * gridSpacingOffset,0, z * gridSpacingOffset) + gridOrgin;
+                Vector3 spawnNodePosition = new Vector3(x * gridSpacingOffset,1, z * gridSpacingOffset) + gridOrgin;
+                spawnGrid(spawnGridPosition, Quaternion.identity);
+                spawnNode(spawnNodePosition, Quaternion.identity);
             }
         }
     }
@@ -34,5 +42,8 @@ public class GridGenerator : MonoBehaviour
     {
         GameObject clone = Instantiate(gridPrefab, positionToSpawn, rotationToSpawn);
     }
-    
+    void spawnNode(Vector3 poitionToSpawn, Quaternion rotationToSpawn)
+    {
+        GameObject clone = Instantiate(nodePrefab, poitionToSpawn, rotationToSpawn);
+    }
 }
